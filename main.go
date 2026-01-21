7029a2ac03cb4439a1184d78b17e255af13a5188
vrmelo <akandeabiodoun@gmail.com>
2026-01-21 20:44:12 +0100

    repository cleaned and restructured

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+)
+func main() {
+	file := flag.String("file", "", "The data file path")
+	crop := flag.String("crop", "all", "The crop you want to analyse specifly")
+	year := flag.String("year", "all", "The year in this format YYYY-MM-DD")
+	report := flag.String("report", "text", "The output format json|text")
+
+	flag.Parse()
+
+	File := *file
+	Crop := *crop
+	Year := *year
+	Report := *report
+
+	fmt.Println(Crop)
+	fmt.Println(Year)
+	fmt.Println(Report)
+	harvs := retrieveData(File)
+	fmt.Println(harvs)
+}
--- parser.go
+++ parser.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"flag"
-	"fmt"
 	"os"
 	"strconv"
 	"time"
@@ -69,25 +67,4 @@ func retrieveData(path string) []Harvest {
 		i++
 	}
 	return harvis
-}
-
-func main() {
-	file := flag.String("file", "", "The data file path")
-	crop := flag.String("crop", "all", "The crop you want to analyse specifly")
-	year := flag.String("year", "all", "The year in this format YYYY-MM-DD")
-	report := flag.String("report", "text", "The output format json|text")
-
-	flag.Parse()
-
-	File := *file
-	Crop := *crop
-	Year := *year
-	Report := *report
-
-	fmt.Println(Crop)
-	fmt.Println(Year)
-	fmt.Println(Report)
-	harvs := retrieveData(File)
-	fmt.Println(harvs)
-
 }
